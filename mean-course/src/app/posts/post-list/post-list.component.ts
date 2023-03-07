7008baaa2386094c8878112b1733482559dbef30
Bravo1B9 <b1@xgmi.com>
2023-03-07 10:48:20 +0000

    Create posts subscription

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,25 +1,34 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
-
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: "First post", content: "This is the first post\s content" },
   //   { title: "Second post", content: "This is the Second post\s content" },
   //   { title: "Third post", content: "This is the Third post\s content" },
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-  constructor(public postsService: PostsService) {
-    
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
 }
