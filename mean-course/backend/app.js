5561f9de21c3f3c85d65f7a6af31168b0404ea5e
Bravo1B9 <b1@xgmi.com>
2023-03-08 13:16:15 +0000

    Create /api/posts enpoint, with dummy data

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: '8ad477add', title: 'First server-side Post', content: 'This is coming from the server' },
+    { id: '86jlajwdk', title: 'Second server-side Post', content: 'This is also coming from the server' }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
