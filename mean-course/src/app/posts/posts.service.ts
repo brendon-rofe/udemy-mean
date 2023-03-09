1037689fd5c610bfe72529fcbb1e2cc94810d90a
Bravo1B9 <b1@xgmi.com>
2023-03-09 04:55:51 +0000

    Create POST end point

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
-import { Post } from "./post.model";
+import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -12,7 +12,10 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('https://3000-bravo1b9-udemymean-a4uesjyj1km.ws-eu89b.gitpod.io/api/posts')
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'https://3000-bravo1b9-udemymean-a4uesjyj1km.ws-eu89b.gitpod.io/api/posts'
+      )
       .subscribe((postData) => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
@@ -24,8 +27,14 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {id: null, title, content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const post: Post = { id: null, title, content };
+    this.http.post<{ message: string }>(
+      'https://3000-bravo1b9-udemymean-a4uesjyj1km.ws-eu89b.gitpod.io/api/posts',
+      post
+    ).subscribe((responseData) => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
