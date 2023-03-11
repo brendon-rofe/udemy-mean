65baac165bf9b45fb6d06999fc2ac818b835ddf1
Bravo1B9 <b1@xgmi.com>
2023-03-11 08:42:12 +0000

    Initial fetching of data

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -36,13 +36,11 @@ app.post('/api/posts', (req, res, next) => {
 })
 
 app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: '8ad477add', title: 'First server-side Post', content: 'This is coming from the server' },
-    { id: '86jlajwdk', title: 'Second server-side Post', content: 'This is also coming from the server' }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
   });
 });
 
