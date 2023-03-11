76439316ed0ffef91c2271320fe6dbdfdfb439b1
Bravo1B9 <b1@xgmi.com>
2023-03-11 08:24:58 +0000

    Check db connection and save post to db

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Brendon:pass123@cluster0.z4xmedo.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database!')
+  })
+  .catch(() => {
+    console.log('Connection failed!')
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +29,7 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
