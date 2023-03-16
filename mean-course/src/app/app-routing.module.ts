83605dfd362bd3618a8a0840768d03f3ae644eec
Brendon Rofe <b1@xgmi.com>
2023-03-16 09:31:53 +0200

    Initial posts component setup

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { PostListComponent } from "./posts/post-list/post-list.component";
 
 const routes: Routes = [
-  
+  { path: '', component: PostListComponent },
 ];
 
 @NgModule({
--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
